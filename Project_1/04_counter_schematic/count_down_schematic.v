841a4e2eea49e437ea4051c1ed45ca751369a2c2
Denny Ly <denny247@gmail.com>
2016-01-14 02:35:18 -0800

    ST pictures done

--- Project_1/04_counter_schematic/count_down_schematic.v
+++ Project_1/04_counter_schematic/count_down_schematic.v
@@ -17,11 +17,7 @@
 // VERSION		"Version 14.0.0 Build 200 06/17/2014 SJ Web Edition"
 // CREATED		"Thu Jan 14 00:25:45 2016"
 
-module count_down_schematic(
-	CLOCK,
-	RESET,
-	out
-);
+module count_down_schematic(CLOCK, RESET, out);
 
 
 input wire	CLOCK;
@@ -128,4 +124,35 @@ assign	SYNTHESIZED_WIRE_19 =  ~SYNTHESIZED_WIRE_21;
 assign	SYNTHESIZED_WIRE_20 =  ~SYNTHESIZED_WIRE_23;
 
 
+endmodule
+
+/*
+The DFlipFlop module is a D_Flip_Flop.  The code was given by James K. Peckol for EE371.
+
+TYPE      |NAME       |WIDTH    |DESCRIPTION       
+-----------------------------------------------------------------------------------
+output    |q          |1 bit    |The present state.
+output    |qBar       |1 bit    |The inverted present state.
+input     |D          |1 bit    |The new state.
+input     |clk        |1 bit    |The clock for the system.
+input     |rst        |1 bit    |The reset signal (active low).
+ */
+module DFlipFlop(q, qBar, D, clk, rst);
+
+  /* Defining the output/input ports */
+  input D, clk, rst;
+  output q, qBar;
+  reg q;
+  
+  /* retrieve inverted output */
+  not n1 (qBar, q);
+  
+  /* update the present state with the next state */
+  always@ (negedge rst or posedge clk) begin
+  if(!rst)
+    q <= 0;
+  else
+    q = D;
+  end
+  
 endmodule
