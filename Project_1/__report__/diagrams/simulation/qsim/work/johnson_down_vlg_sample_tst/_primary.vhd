85fb53864c5b853742352d02ddb8308d2b111bce
Minh Khuu <minhkhuu@uw.edu>
2016-01-15 23:48:37 -0800

    remove work files and finalize project 1 folder

--- Project_1/__report__/diagrams/simulation/qsim/work/johnson_down_vlg_sample_tst/_primary.vhd
+++
@@ -1,9 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity johnson_down_vlg_sample_tst is
-    port(
-        CLOCK           : in     vl_logic;
-        RESET           : in     vl_logic;
-        sampler_tx      : out    vl_logic
-    );
-end johnson_down_vlg_sample_tst;
