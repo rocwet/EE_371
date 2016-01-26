3028bc364cbac4e92d51e3dee5856d7eebb7f515
dennyly <denny247@gmail.com>
2016-01-25 18:42:15 -0800

    Basic Testbench done

--- Project_2/inputHandler.v
+++ Project_2/inputHandler.v
@@ -7,39 +7,37 @@
 // reset             = reset signal
 // button            = received button press
 // BT                = new button signal w/ implemented rules
-module inputHandler(BT, Clock, reset, button);
+module inputHandler(out, Clock, reset, button);
 
 	// ports
 	input Clock, reset, button;
-	output BT;
+	output reg out;
 	
 	// variables
-	reg btn;
-	parameter [1:0]  A = 2'b00, B = 2'b01, C = 2'b10;
-	wire [1:0] PS;
-	reg [1:0] NS;
-	
-	// assignemnts
-	assign BT = btn;
+	parameter[1:0]  A = 2'b00, B = 2'b01, C = 2'b10;
+	wire[1:0] PS;
+	reg[1:0] NS;
 	
 	// next state logic
-	always @(PS or button)
+	always @(PS or button) begin
 		case(PS)
-			A:	if(button) NS = B;
-				else NS = A;
-			B: if(button) NS = C;
-				else NS = A;
-			C: if(button) NS = C;
-				else NS = A;
+			A:	if(button) 	NS = B;
+				else 			NS = A;
+			B: if(button) 	NS = C;
+				else 			NS = A;
+			C: if(button) 	NS = C;
+				else 			NS = A;
 		endcase
+	end
 		
 	// output logic
-	always @ (PS)
+	always @(PS) begin
 		case(PS)
-			A: btn = 0;
-			B: btn = 1;
-			C: btn = 0;
+			A: out = 0;
+			B: out = 1;
+			C: out = 0;
 		endcase
+	end
 	
 	// state holding changing
 	DFlipFlop msb (.q(PS[0]), .qBar(), .D(NS[0]), .clk(Clock), .rst(reset));
